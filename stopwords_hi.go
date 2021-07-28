b728bf7e9e43a5d723831460b17d09fdd20e2737
Shashank Sachan <46921852+shashank-sachan@users.noreply.github.com>
2021-07-28 19:52:26 +0530

    Create stopwords_hi.go

---
+++ stopwords_hi.go
@@ -0,0 +1,7 @@
+// Copyright (c) 2005, Jacques Savoy.
+// Use of this source code is governed by the BSD license
+// license that can be found in the LICENSE file.
+
+package stopwords
+
+var hindi = map[string]string{}
